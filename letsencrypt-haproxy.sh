f4093c170de8a5450f694effe0aae7df97faa3e8
Mickael Stanslas <contact@mickael-stanislas.com>
2018-03-10 12:58:04 +0100

    Update Release

--- letsencrypt-haproxy.sh
+++ letsencrypt-haproxy.sh
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
-# Last Update : 20180310 - Version 1.0
+# Last Update : 20180310 - Version 0.1-BETA
 #
 # --- Variable -----------------------------------------------------------------
 sendmail="false" # -- Send mail if error in execution script
